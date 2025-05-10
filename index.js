d8c4c7bdc53fd42eacaf8eca54cf59662e3f2b52
Rokhshana ahmadzia <Rokhshana.ah2024@gmail.com>
2025-05-10 23:45:56 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const proverbsRoutes = require('./routes/proverbs');
 const app= express(); 
-const PORT = 300;
+const PORT = 3000;
 
 app.use(express.json());
 app.use('/proverbs',proverbsRoutes);
